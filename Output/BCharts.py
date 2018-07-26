082344ef20de1a0a78f68b7515e339c7a535e9ca
Bill Magill <35304879+wmagill4@users.noreply.github.com>
2018-07-26 19:11:55 -0400

    CleanedUp
    moved my charts

--- Output/BCharts.py
+++
@@ -1,9 +0,0 @@
-#dependencies
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
-#read CSV
-education_data = pd.read_csv("../Output/Edu_Data.csv")
-education_data.head()
-for row in education_data
